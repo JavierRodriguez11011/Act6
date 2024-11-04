180718afd8bf477d4586b7cf642ddae7f99eb354
JavierRodriguez11011 <159870868+JavierRodriguez11011@users.noreply.github.com>
2024-11-04 01:28:09 -0600

    ya quedo

--- act6.c
+++ act6.c
@@ -14,25 +14,20 @@ int main() {
   stdio_init_all();
   setup_gpios();
 
-  const char *words[] = {"SSD1306", "DISPLAY", "DRIVER"};
 
-  ssd1306_t disp;
-  disp.external_vcc = false;
-  ssd1306_init(&disp, 128, 64, 0x3C, i2c0);
-  ssd1306_clear(&disp);
-
-  for (int y = 0; y < 31; ++y) {
-    ssd1306_draw_line(&disp, 0, y, 127, y);
-    ssd1306_show(&disp);
-    sleep_ms(SLEEPTIME);
-    ssd1306_clear(&disp);
-  }
+  ssd1306_t oledd;
+  oledd.external_vcc = false;
+  ssd1306_init(&oledd, 128, 64, 0x3C, i2c0);
+  ssd1306_clear(&oledd);
+const char *nombre = "Javier Rodriguez Meneses";
+ssd1306_draw_string(&oledd, 0, 0, 1, nombre);
+ssd1306_draw_line(&oledd, 0, 10, 127, 10);
+  ssd1306_show(&oledd);
 }
-
 void setup_gpios(void) {
   i2c_init(i2c0, 100000);
-  gpio_set_function(4, GPIO_FUNC_I2C);
-  gpio_set_function(5, GPIO_FUNC_I2C);
-  gpio_pull_up(4);
-  gpio_pull_up(5);
+  gpio_set_function(0, GPIO_FUNC_I2C);
+  gpio_set_function(1, GPIO_FUNC_I2C);
+  gpio_pull_up(0);
+  gpio_pull_up(1);
 }
